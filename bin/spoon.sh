9f0322d70827d108b117349713aea9b714f8e631
tbadard <tbadard@b4ed45c1-2b81-45f6-8f82-25a0ccef0984>
2012-01-04 20:42:13 +0000

    Fix a bug with the welcome page on some Linux versions. Now, GeoKettle does not open a new firefox window or tab anymore. The welcome page is displayed as a tab in the GeoKettle window as in any other OSes.

--- bin/spoon.sh
+++ bin/spoon.sh
@@ -9,10 +9,11 @@
 # set MOZILLA_FIVE_HOME=/usr/local/mozilla
 # set LD_LIBRARY_PATH=/usr/local/mozilla
 
-set MOZILLA_FIVE_HOME=/usr/lib/xulrunner-1.9.1.9/
-set LD_LIBRARY_PATH=${MOZILLA_FIVE_HOME}:${LD_LIBRARY_PATH}
+MOZILLA_FIVE_HOME=$(find /usr/lib -maxdepth 1 -name xulrunner-[0-9]* | tail -1)
+LD_LIBRARY_PATH=${MOZILLA_FIVE_HOME}:${LD_LIBRARY_PATH}
 export MOZILLA_FIVE_HOME LD_LIBRARY_PATH
 export GDK_NATIVE_WINDOWS=true 
+export LIBOVERLAY_SCROLLBAR=0
 
 # **************************************************
 # ** Libraries used by Kettle:                    **
