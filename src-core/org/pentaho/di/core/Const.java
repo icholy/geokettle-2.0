4b1b4d0d3062c4907c600484ee298a68b1908a7f
tbadard <tbadard@b4ed45c1-2b81-45f6-8f82-25a0ccef0984>
2013-02-02 23:34:04 +0000

    Changes version number to 2.5 and updates copyright mentions.

--- src-core/org/pentaho/di/core/Const.java
+++ src-core/org/pentaho/di/core/Const.java
@@ -53,7 +53,7 @@ public class Const
 	/**
 	 *  Version number
 	 */
-	public static final String VERSION = "2.0";
+	public static final String VERSION = "2.5";
 
 	/**
 	 * Sleep time waiting when buffer is empty
